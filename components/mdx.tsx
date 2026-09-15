546560f4e6f88fff5e00f3c7db4782f287e3d73d
Eu sou brasileiro <zynkore.ofc@gmail.com>
2026-09-15 20:09:56 -0300

    Update mdx.tsx

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,7 +1,6 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import { Accordion, Accordions } from 'fumadocs-ui/components/accordion';
 import type { MDXComponents } from 'mdx/types';
-
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
