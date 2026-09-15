4044cf53b359c633bd42d8f38bb4990d1a356b5f
Eu sou brasileiro <zynkore.ofc@gmail.com>
2026-09-15 17:45:58 -0300

    Update GitHub user and repo information

--- lib/shared.ts
+++ lib/shared.ts
@@ -7,8 +7,8 @@ export const docsContentRoute = '/llms.mdx/docs';
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: 'fuma-nama',
-  repo: 'fumadocs',
+  user: 'Zynkore',
+  repo: 'Hub',
   branch: 'main',
 };
 
